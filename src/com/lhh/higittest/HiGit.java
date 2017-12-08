72600c639457e6387060a235684826b72ff15e01
Lan <lanhonghui100@gmail>
2017-12-08 10:46:42 +0800

    做一些修改

--- src/com/lhh/higittest/HiGit.java
+++ src/com/lhh/higittest/HiGit.java
@@ -3,5 +3,8 @@ package com.lhh.higittest;
 public class HiGit {
     public static void main(String[] args) {
         System.out.println("Hi GitHub!!!");
+        System.out.println("Hi GitHub!!!");
+        System.out.println("Hi GitHub!!!");
+        System.out.println("Hi GitHub!!!");
     }
 }
