5e3e922ed447157af07bd2afcc93c0da53dac663
Lan <lanhonghui100@gmail>
2017-12-08 10:48:32 +0800

    第一次用idea上传项目

--- src/com/lhh/higittest/HiGit.java
+++ src/com/lhh/higittest/HiGit.java
@@ -6,5 +6,7 @@ public class HiGit {
         System.out.println("Hi GitHub!!!");
         System.out.println("Hi GitHub!!!");
         System.out.println("Hi GitHub!!!");
+        System.out.println("Hi GitHub!!!");
+        System.out.println("Hi GitHub!!!");
     }
 }
